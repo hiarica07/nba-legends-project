3078dd261f483133050955f11e19eb6a5c89bcc2
hiarica07 <halilarica@gmail.com>
2024-07-15 16:02:22 +0300

    finito

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,53 +1,54 @@
-import PlayerCard from './PlayerCard';
-import  { data }  from '../helper/data'; 
-import { useState } from 'react';
+import React, { useState } from 'react'
+import PlayerCard from './PlayerCard'
+import { data } from "../helper/data"
+
+
 
 const CardContainer = () => {
 
- const [search, setSearch] = useState("")
+    const [search, setSearch] = useState("")
 
- const handleSearchChange = (e) => {
-  // console.log(e.target.value)
-  setSearch(e.target.value);
-};
-const filteredPlayers = data.filter((e) =>
-  e.name.toLowerCase().includes(search.toLowerCase()))
-  
+    const handleSearchChange = (e) => {
+        console.log(e.target.value);
+        setSearch(e.target.value)
+    }
+
+    const handleClearSearch = () => {
+        setSearch("");
+      };
+
+    const filteredPlayers = data.filter((e)=> e.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+
+    
+    
   return (
     <div>
-
-    <div className="search-box">
-      <input
-       type="text"
-       placeholder='Search'
-       className='search-input'
-       value={search}
-       onChange={handleSearchChange}
-        />
-        <div></div>
-    </div>
-    
-
-
-
-    
-    <div className="card-container">
-        {filteredPlayers.map((player, index) => (
-          <PlayerCard key={index} {...player} />
+        <div className='search-box'>
+            <input 
+                type="text" 
+                id="" 
+                placeholder="Search for a player..."
+                className='search-input'
+                value={search}
+                onChange={handleSearchChange}
+            />
+            {search && (
+          <span
+            className='clear-icon'
+            onClick={handleClearSearch}            
+          >
+            &times;
+          </span>
+        )}
+        </div>
+        <div className="card-container">
+        {filteredPlayers.map((player, index)=>(
+            <PlayerCard key={index} {...player}/>
         ))}
-      </div>
-      
-      
-      
-      
-      
-      
-      </div>
+            
+        </div>
+    </div>
+  )
+}
 
-    
-   
-    
-  );
-};
-
-export default CardContainer;
+export default CardContainer
